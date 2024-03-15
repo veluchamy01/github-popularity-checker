3e56622e1d9f11ac8e9c37efe6194e8f3134e6ff
Veluchamy01 <mr.veluchamy@gmail.com>
2024-03-15 04:54:33 +0100

    initial commit

---
+++ src/main/java/com/orderbird/github/popularity/checker/GithubPopularityCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.orderbird.github.popularity.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPopularityCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubPopularityCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderbird/github/popularity/checker/GithubPopularityCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderbird.github.popularity.checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPopularityCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
